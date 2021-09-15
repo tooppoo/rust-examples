2342c5081879b49547fd3262751f1a31d6bff76d
philomagi <tooppoo56@gmail.com>
2021-09-15 09:17:06 +0900

    not print secret number

--- tutorial/guessing_game/src/main.rs
+++ tutorial/guessing_game/src/main.rs
@@ -6,7 +6,6 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("Secret number is {}", secret_number);
 
     loop {
         println!("Please input your guess.");
