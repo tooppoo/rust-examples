670abe5172b63169896b0d0c8087a89f7e113f8c
philomagi <tooppoo56@gmail.com>
2021-09-15 09:32:52 +0900

    shadowing

--- tutorial/variables/src/main.rs
+++ tutorial/variables/src/main.rs
@@ -1,10 +1,20 @@
 fn main() {
     mutate();
+    shadowing();
 }
 
 fn mutate() {
     let mut x = 5;
-    println!("value of x is {}", x);
+    println!("mutate:value of x is {}", x);
     x = 6;
-    println!("value of x is {}", x);
+    println!("mutate:value of x is {}", x);
+}
+
+fn shadowing() {
+    let x = 5;
+    println!("shadowing:value of x is {}", x);
+    let x = x + 3;
+    println!("shadowing:value of x is {}", x);
+    let x = x * 2;
+    println!("shadowing:value of x is {}", x);
 }
