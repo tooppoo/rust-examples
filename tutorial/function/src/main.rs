8a42fdef8f38772bbeb5b11a0817345bd5df9d73
philomagi <tooppoo56@gmail.com>
2021-09-17 03:16:53 +0900

    use block as expression

--- tutorial/function/src/main.rs
+++ tutorial/function/src/main.rs
@@ -2,6 +2,14 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5, 6);
+
+    // let x = 5;
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("value of y is {}", y);
 }
 
 fn another_function(x: i32, y: i32) {
