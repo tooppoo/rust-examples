64327ccf04df3c6917cf4bbfdc85bef83b1cf111
philomagi <tooppoo56@gmail.com>
2021-09-17 03:14:20 +0900

    add argument

--- tutorial/function/src/main.rs
+++ tutorial/function/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function(5);
+    another_function(5, 6);
 }
 
-fn another_function(x: i32) {
-    println!("value of x is {}", x);
+fn another_function(x: i32, y: i32) {
+    println!("value of x is {}, y is {}", x, y);
 }
