7d56ce5d1cb6fc94a5f80f795884948fda864960
philomagi <tooppoo56@gmail.com>
2021-09-16 20:12:50 +0900

    add tuple

--- tutorial/data_type/src/main.rs
+++ tutorial/data_type/src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("guess is {}", guess);
+
+    let x = 2.0;
+    let y: f32 = 3.0;
+    let z: f64 = x;
+    println!("x is {}, y is {}, z is {}", x, y, z);
+
+    let sum: i32 = 5 + 10;
+    let difference: f64 = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient: f32 = 56.7 / 32.2;
+    let remainder: usize = 43 % 5;
+    println!(
+        "sum is {}, difference is {}, product is {}, quotient is {}, remainde is {}",
+        sum, difference, product, quotient, remainder
+    );
+
+    let t = true;
+    let f: bool = false;
+    println!("t is {}, f is {}", t, f);
+
+    let c = 'z';
+    let z: char = 'Z';
+    let heart_eyed_cat: char = '😻';
+    println!("c is {}, z is {}, cat is {}", c, z, heart_eyed_cat);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 255);
+    let (x, y, z) = tup;
+    println!("tup is ({}, {}, {})", x, y, z);
+    println!("tup is ({}, {}, {})", tup.0, tup.1, tup.2);
 }
