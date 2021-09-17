3a7f31c3595f4acc300e7b59ff755eb15fb9e885
philomagi <tooppoo56@gmail.com>
2021-09-17 18:15:45 +0900

    add function

--- tutorial/function/src/main.rs
+++ tutorial/function/src/main.rs
@@ -13,6 +13,9 @@ fn main() {
 
     let x = five();
     println!("value of x is {}", x);
+
+    let x = plus_one(5);
+    println!("value of x is {}", x)
 }
 
 fn another_function(x: i32, y: i32) {
@@ -21,4 +24,8 @@ fn another_function(x: i32, y: i32) {
 
 fn five() -> i32 {
     5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
