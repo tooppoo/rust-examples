ac0ee7e2b7b3b88625bd691e302599bc785a6a00
philomagi <tooppoo56@gmail.com>
2021-11-01 06:44:20 +0900

    test boroowing

--- tutorial/04_2_references-and-borrowing/src/main.rs
+++ tutorial/04_2_references-and-borrowing/src/main.rs
@@ -3,8 +3,16 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("the length of '{}' is  {}.", s1, len);
+
+    let mut s = String::from("hello");
+    println!("s before change is '{}'", s);
+    change_borrowing(&mut s);
+    println!("s after change is '{}'", s);
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+fn change_borrowing(s: &mut String) {
+    s.push_str(", world!");
 }
