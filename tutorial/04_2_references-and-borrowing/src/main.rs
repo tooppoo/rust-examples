3f67ad718fe3b29c7599b376cbe2612268acff74
philomagi <tooppoo56@gmail.com>
2021-11-01 06:28:58 +0900

    test reference

--- tutorial/04_2_references-and-borrowing/src/main.rs
+++ tutorial/04_2_references-and-borrowing/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("calculate length");
+    let len = calculate_length(&s1);
+
+    println!("the length of '{}' is  {}.", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
