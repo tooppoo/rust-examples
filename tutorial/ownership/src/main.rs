eb22cbff94726b897bd3432662b3fbd7cbf7c053
philomagi <tooppoo56@gmail.com>
2021-11-01 06:22:38 +0900

    ownership test

--- tutorial/ownership/src/main.rs
+++ tutorial/ownership/src/main.rs
@@ -6,16 +6,16 @@ fn main() {
     let s1 = "hello";
     let s2 = s1;
 
-    println!("{}, world!", s1);
-    println!("{}, world!", s2);
+    println!("s1 {}, world!", s1);
+    println!("s2 {}, world!", s2);
 
     let s1 = String::from(s1);
     let s2 = s1.clone();
     let s3 = s1;
 
     // println!("{}, world!", s1); error
-    println!("{}, world!", s2);
-    println!("{}, world!", s3);
+    println!("s2 {}, world!", s2);
+    println!("s3 {}, world!", s3);
 
     let s = String::from("hello");
 
@@ -24,7 +24,13 @@ fn main() {
 
     let x = 5;
     makes_copy(x);
-    println!("{}", x);
+    println!("x = {}", x);
+
+    let s1 = gives_ownership();
+    println!("s1 = {}", s1);
+    let s2 = String::from("takes and gives back");
+    let s3 = takes_and_gives_back(s2);
+    println!("s3 = {}", s3);
 }
 
 fn takes_ownership(s: String) {
@@ -32,4 +38,12 @@ fn takes_ownership(s: String) {
 }
 fn makes_copy(i: i32) {
     println!("{}", i);
+}
+fn gives_ownership() -> String {
+    let s = String::from("give ownership");
+
+    s
+}
+fn takes_and_gives_back(s: String) -> String {
+    s
 }
