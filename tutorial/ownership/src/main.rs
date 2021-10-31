460c27da06780b57828dd538e45dd55363e4dbc1
philomagi <tooppoo56@gmail.com>
2021-11-01 04:53:01 +0900

    ownership

--- tutorial/ownership/src/main.rs
+++ tutorial/ownership/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let y = x;
+    println!("x = {}, y = {}", x, y);
+
+    let s1 = "hello";
+    let s2 = s1;
+
+    println!("{}, world!", s1);
+    println!("{}, world!", s2);
+
+    let s1 = String::from(s1);
+    let s2 = s1.clone();
+    let s3 = s1;
+
+    // println!("{}, world!", s1); error
+    println!("{}, world!", s2);
+    println!("{}, world!", s3);
 }
