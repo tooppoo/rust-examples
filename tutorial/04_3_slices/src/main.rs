8fd7a7bc1aa6c48fb87298fa9f81d873a1adae10
philomagi <tooppoo56@gmail.com>
2021-11-03 04:26:06 +0900

    impl first_word

--- tutorial/04_3_slices/src/main.rs
+++ tutorial/04_3_slices/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello, world!");
+    let first_at = first_word(&s);
+
+    println!("first of '{}' is {}", s, first_at);
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
