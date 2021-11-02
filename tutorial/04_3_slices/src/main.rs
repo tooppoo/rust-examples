3659b930acf482a9536a2f2a4dfc8f352cd8a05d
philomagi <tooppoo56@gmail.com>
2021-11-03 05:43:22 +0900

    rename

--- tutorial/04_3_slices/src/main.rs
+++ tutorial/04_3_slices/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
     let mut s = String::from("Hello, world!");
-    let first_at = first_word(&s);
+    let first_at = first_word_at(&s);
 
     s.clear();
 
     println!("first of '{}' is {}", s, first_at);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word_at(s: &String) -> usize {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
