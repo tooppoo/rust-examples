dfd8cd60d372cb7a05a55bf9051d2027cbfe8349
philomagi <tooppoo56@gmail.com>
2021-11-04 07:17:12 +0900

    define message by enum

--- tutorial/06_1_defininig-an-enum/src/main.rs
+++ tutorial/06_1_defininig-an-enum/src/main.rs
@@ -4,9 +4,24 @@ enum IpAddressKind {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+impl Message {
+    fn call(&self) -> &str {
+        &"some message"
+    }
+}
+
 fn main() {
     route(IpAddressKind::V4(127,0,0,0));
     route(IpAddressKind::V6(String::from("::1")));
+
+    let m: Message = Message::Move { x: 10, y: 20 };
+    println!("{}", m.call());
 }
 
 fn route(_ip: IpAddressKind) { }
