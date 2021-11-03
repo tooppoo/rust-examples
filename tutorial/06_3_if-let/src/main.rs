20cdf400cbdaf339fa7e2f99611129bf0748f058
philomagi <tooppoo56@gmail.com>
2021-11-04 08:37:25 +0900

    add if-let sample

--- tutorial/06_3_if-let/src/main.rs
+++ tutorial/06_3_if-let/src/main.rs
@@ -4,4 +4,9 @@ fn main() {
         Some(3) => println!("three"),
         _ => (),
     }
+
+    let some_u8_value = Some(3u8);
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
 }
