050fd3e5c329860bf50605e1c45528d851b385ae
philomagi <tooppoo56@gmail.com>
2021-11-04 05:41:20 +0900

    add method

--- tutorial/05_3_method-syntax/src/main.rs
+++ tutorial/05_3_method-syntax/src/main.rs
@@ -21,4 +21,11 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         rect.area()
     );
+
+    let r1 = Rectangle { width: 30, height: 50 };
+    let r2 = Rectangle { width: 10, height: 40 };
+    let r3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can r1 hold r2? {}", r1.can_hold(&r2));
+    println!("Can r1 hold r3? {}", r1.can_hold(&r3));
 }
