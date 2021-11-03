dbc99a85ac9c1af4b10d2526be23d5e9a0bb2f49
philomagi <tooppoo56@gmail.com>
2021-11-04 07:10:01 +0900

    set loopback

--- tutorial/06_1_defininig-an-enum/src/main.rs
+++ tutorial/06_1_defininig-an-enum/src/main.rs
@@ -1,12 +1,12 @@
 
 enum IpAddressKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
 fn main() {
-    route(IpAddressKind::V4);
-    route(IpAddressKind::V6);
+    route(IpAddressKind::V4(String::from("127.0.0.1")));
+    route(IpAddressKind::V6(String::from("::1")));
 }
 
 fn route(_ip: IpAddressKind) { }
