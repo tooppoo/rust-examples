20ea2ea593a117d2479fcf2b70c3fb5234063698
philomagi <tooppoo56@gmail.com>
2021-11-04 07:24:22 +0900

    print coin

--- tutorial/06_2_match/src/main.rs
+++ tutorial/06_2_match/src/main.rs
@@ -1,3 +1,23 @@
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("penny = {}c", value_in_cents(Coin::Penny));
+    println!("nickel = {}c", value_in_cents(Coin::Nickel));
+    println!("dime = {}c", value_in_cents(Coin::Dime));
+    println!("quarter = {}c", value_in_cents(Coin::Quarter));
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
