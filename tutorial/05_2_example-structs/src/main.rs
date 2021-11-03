8d5ffef407f2387ce1d7036ea9dc0971e364d350
philomagi <tooppoo56@gmail.com>
2021-11-04 05:30:09 +0900

    support debug print of rectanble

--- tutorial/05_2_example-structs/src/main.rs
+++ tutorial/05_2_example-structs/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -9,7 +10,7 @@ fn main() {
         height: 50,
     };
 
-    println!("rect is {}", rect);
+    println!("rect is {:#?}", rect);
 
     println!(
         "The area of the rectangle is {} square pixels.",
