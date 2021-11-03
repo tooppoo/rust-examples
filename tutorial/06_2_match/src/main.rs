ee9d8f11e0e125ca5b48e2d5f396440db9828043
philomagi <tooppoo56@gmail.com>
2021-11-04 08:30:01 +0900

    get cent via method

--- tutorial/06_2_match/src/main.rs
+++ tutorial/06_2_match/src/main.rs
@@ -10,23 +10,24 @@ enum Coin {
     Dime,
     Quarter(UsState),
 }
+impl Coin {
+    fn to_cents(&self) -> u32 {
+        match self {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("state quarter from {:?}", state);
 
-fn main() {
-    println!("penny = {}c", value_in_cents(Coin::Penny));
-    println!("nickel = {}c", value_in_cents(Coin::Nickel));
-    println!("dime = {}c", value_in_cents(Coin::Dime));
-    println!("quarter = {}c", value_in_cents(Coin::Quarter(UsState::Alabama)));
+                25
+            },
+        }  
+    }
 }
 
-fn value_in_cents(coin: Coin) -> u32 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("state quarter from {:?}", state);
-
-            25
-        },
-    }
+fn main() {
+    println!("penny = {}c", Coin::Penny.to_cents());
+    println!("nickel = {}c", Coin::Nickel.to_cents());
+    println!("dime = {}c", Coin::Dime.to_cents());
+    println!("quarter = {}c", Coin::Quarter(UsState::Alabama).to_cents());
 }
