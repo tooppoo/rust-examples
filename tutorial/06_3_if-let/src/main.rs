f3b8770d2c9d632e541c0dc248e596190c365feb
philomagi <tooppoo56@gmail.com>
2021-11-04 09:19:45 +0900

    use if-let

--- tutorial/06_3_if-let/src/main.rs
+++ tutorial/06_3_if-let/src/main.rs
@@ -29,6 +29,14 @@ fn main() {
     let coin = Coin::Dime;
     let count = count_up_if_quarter_via_match(coin, 0);
     println!("count is {}", count);
+
+    let coin = Coin::Quarter(UsState::Alabama);
+    let count = count_up_if_quarter_via_if_let(coin, 0);
+    println!("count is {}", count);
+
+    let coin = Coin::Dime;
+    let count = count_up_if_quarter_via_if_let(coin, 0);
+    println!("count is {}", count);
 }
 
 fn count_up_if_quarter_via_match(coin: Coin, count: u32) -> u32 {
@@ -39,5 +47,15 @@ fn count_up_if_quarter_via_match(coin: Coin, count: u32) -> u32 {
             count
         },
         _ => count + 1,
+    }
+}
+
+fn count_up_if_quarter_via_if_let(coin: Coin, count: u32) -> u32 {
+    if let Coin::Quarter(state) = coin {
+        println!("state quarter from {:?}", state);
+
+        count
+    } else {
+        count + 1
     }
 }
