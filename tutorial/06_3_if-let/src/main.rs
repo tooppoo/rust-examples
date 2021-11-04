9a9fe201483870464b012a8f0c7faff7ae24af07
philomagi <tooppoo56@gmail.com>
2021-11-04 09:17:33 +0900

    count up via match

--- tutorial/06_3_if-let/src/main.rs
+++ tutorial/06_3_if-let/src/main.rs
@@ -1,3 +1,15 @@
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
     let some_u8_value = Some(0u8);
     match some_u8_value {
@@ -8,5 +20,24 @@ fn main() {
     let some_u8_value = Some(3u8);
     if let Some(3) = some_u8_value {
         println!("three");
+    }
+
+    let coin = Coin::Quarter(UsState::Alabama);
+    let count = count_up_if_quarter_via_match(coin, 0);
+    println!("count is {}", count);
+
+    let coin = Coin::Dime;
+    let count = count_up_if_quarter_via_match(coin, 0);
+    println!("count is {}", count);
+}
+
+fn count_up_if_quarter_via_match(coin: Coin, count: u32) -> u32 {
+    match coin {
+        Coin::Quarter(state) => {
+            println!("state quarter from {:?}", state);
+
+            count
+        },
+        _ => count + 1,
     }
 }
